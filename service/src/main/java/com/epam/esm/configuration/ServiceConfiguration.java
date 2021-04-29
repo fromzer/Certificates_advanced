5baf3543cbefa8db27cb7d4f3aa79266aed3b318
Egor <fromzer@mail.ru>
2021-04-29 16:54:15 +0300

    return configuration

--- service/src/main/java/com/epam/esm/configuration/ServiceConfiguration.java
+++ service/src/main/java/com/epam/esm/configuration/ServiceConfiguration.java
@@ -1,26 +1,18 @@
 package com.epam.esm.configuration;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan("com.epam.esm")
+@Import(DataConfiguration.class)
 public class ServiceConfiguration {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
-        ms.setBasename("classpath:messages");
-        ms.setDefaultEncoding("UTF-8");
-        return ms;
     }
 }
